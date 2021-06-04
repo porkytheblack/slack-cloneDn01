6d99b64852bcdde1620a5894d60b0d0df312897a
porkytheblack <58218526+porkytheblack@users.noreply.github.com>
2021-06-04 17:28:45 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-    apiKey: "AIzaSyAfOZUomqRAtsPe2o06Sd2kCEtYB4WAfM8",
-    authDomain: "slack-clone-866d2.firebaseapp.com",
-    projectId: "slack-clone-866d2",
-    storageBucket: "slack-clone-866d2.appspot.com",
-    messagingSenderId: "351093833548",
-    appId: "1:351093833548:web:dae9a22df31b50517819e9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore()
